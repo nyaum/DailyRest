d363b4bd4bd3d2012b933f7c6ce370cfeddee83d
문현준 <mhj45072@secuidea.com>
2022-05-10 15:51:39 +0900

    날짜 관련 이슈

--- bot/common.py
+++ bot/common.py
@@ -20,5 +20,13 @@ except:
 
 today = dt.datetime.now()
 nowyear = today.strftime("%Y")
+
+endyear = nowyear + '1231'
+strftoday2 = today.strftime("%Y%m%d")
+# strftoday2 = '20221231'
+
+if endyear == strftoday2:
+    nowyear = int(nowyear) + 1
+
 strftoday = today.strftime("작성일 : %Y년 %m월 %d일 \n\n".encode('unicode-escape').decode())
 strftoday = strftoday.encode().decode('unicode-escape')
